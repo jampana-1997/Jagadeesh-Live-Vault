a058700c1dad1296e41d6a38f81311c9d67dc640
Jampana Sai Jagadeesh <jampana633@gmail.com>
2026-07-21 15:25:22 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,63 @@
+// ===============================
+// LIVE VAULT LOGIN
+// ===============================
+
+// Login Credentials
+const VALID_USERNAME = "Jampana";
+const VALID_PASSWORD = "Jagadeesh";
+
+// Already Logged In
+if (localStorage.getItem("liveVaultLogin") === "true") {
+    window.location.href = "dashboard.html";
+}
+
+const loginForm = document.getElementById("loginForm");
+const errorMessage = document.getElementById("errorMessage");
+
+loginForm.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const username = document
+        .getElementById("username")
+        .value
+        .trim();
+
+    const password = document
+        .getElementById("password")
+        .value
+        .trim();
+
+    if (
+        username === VALID_USERNAME &&
+        password === VALID_PASSWORD
+    ) {
+
+        // Save Login
+        localStorage.setItem("liveVaultLogin", "true");
+
+        // Optional
+        localStorage.setItem("liveVaultUser", username);
+
+        // Redirect
+        window.location.href = "dashboard.html";
+
+    } else {
+
+        errorMessage.innerHTML =
+            "<i class='fa-solid fa-circle-exclamation'></i> Invalid Username or Password";
+
+        document.getElementById("password").value = "";
+
+    }
+
+});
+
+// Press Enter Anywhere
+document.addEventListener("keydown", function (e) {
+
+    if (e.key === "Enter") {
+        loginForm.requestSubmit();
+    }
+
+});
